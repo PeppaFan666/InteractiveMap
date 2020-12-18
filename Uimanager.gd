fe2f609bb02de9ad19dfcffd2304c0510e8093d3
PeppaFan666 <treebarker2007@gmail.com>
2020-12-18 07:41:07 -0700

    HideUi fix and Key

--- Uimanager.gd
+++ Uimanager.gd
@@ -2,21 +2,32 @@ extends Node
 onready var cherokee = get_node("../VBoxContainer/Cherokee")
 onready var sioux = get_node("../VBoxContainer/Sioux")
 onready var info = get_node("../ColorRect")
+onready var arrow = get_node("../arrow")
+onready var control = get_node("../Control")
 onready var uibutton = get_node("../VBoxContainer/HideUI")
 onready var clear = preload("res://ClearShader.tres")
 onready var normal = preload("res://NormalColor.tres")
-var didhide = false
-var clickcount = 0
+func _ready():
+	arrow.hide()
 func _on_HideUI_pressed():
-	uibutton.set_text("Show Ui")
+	control.hide()
 	info.hide()
 	sioux.set_material(clear)
 	cherokee.set_material(clear)
-	print("hidden")
-	didhide = true
-	didhide = true
-	print("shown")
-	uibutton.set_text("Hide Ui")
+	cherokee.disabled = true
+	sioux.disabled = true
+	uibutton.disabled = true
+	uibutton.hide()
+	arrow.show()
+
+
+func _on_arrow_pressed():
+	control.show()
+	uibutton.show()
 	info.show()
 	sioux.set_material(normal)
 	cherokee.set_material(normal)
+	cherokee.disabled = false
+	sioux.disabled = false
+	uibutton.disabled = false
+	arrow.hide()
